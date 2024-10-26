eaebad24c405355b099ec6f95238a05d5ddc36eb
trungdangtd <trungdang1301@gmail.com>
2024-10-26 16:50:46 +0700

    create signin endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 authRouter.post('/api/signup', async (req, res) => {
     try {
@@ -22,5 +23,34 @@ authRouter.post('/api/signup', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
+
+//signin API endpoint
+
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const findUser = await User.findOne({ email });
+        if (!findUser) {
+            res.status(400).json({ msg: "Email không tồn tại" })
+        } else {
+            //kiểm tra xem mật khẩu nhập vào có đúng vs mật khẩu đã mã hoá trong db không
+            const isMatch = await bcrypt.compare(password, findUser.password);
+            if (!isMatch) {
+                res.status(400).json({ msg: "Mật khẩu không đúng" })
+            } else {
+                const token = jwt.sign({ id: findUser._id }, "passwordKey");
+
+                //xoá thông tin nhạy cảm(mật khẩu)
+                const { password, ...userWithoutPassword } = findUser._doc;
+
+                //send the response
+
+                res.json({ token, ...userWithoutPassword });
+            };
+        };
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
 
 module.exports = authRouter;
